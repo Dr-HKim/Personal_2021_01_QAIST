afe3957b5d6f67f0b427899a63449f00388d6e1b
Dr-HKim <yui7890@gmail.com>
2021-04-26 02:00:59 +0900

    Research Note: - minor updates

--- source_codes/main_20210219_BOK_Curve.py
+++ source_codes/main_20210219_BOK_Curve.py
@@ -12,6 +12,16 @@ from bs4 import BeautifulSoup
 from data_raw.def_authentication import *
 
 
+def align_yaxis(ax1, v1, ax2, v2):
+    """adjust ax2 ylimit so that v2 in ax2 is aligned to v1 in ax1"""
+    _, y1 = ax1.transData.transform((0, v1))
+    _, y2 = ax2.transData.transform((0, v2))
+    inv = ax2.transData.inverted()
+    _, dy = inv.transform((0, 0)) - inv.transform((0, y1-y2))
+    miny, maxy = ax2.get_ylim()
+    ax2.set_ylim(miny+dy, maxy+dy)
+
+
 def get_bok_data(STAT_CODE, CYCLE_TYPE, START_DATE, END_DATE, AUTH_KEY=get_bok_auth_key(), REQ_TYPE="xml", LANG="kr",
                  START_COUNT="1", END_COUNT="100000", ITEM_1="?", ITEM_2="?", ITEM_3="?"):
     # 호출하려는 OpenAPI URL 정의
@@ -100,6 +110,103 @@ QQ_START_DATE = "20031"
 DD_END_DATE = "20210219"
 MM_END_DATE = "202101"
 QQ_END_DATE = "20204"
+
+########################################################################################################################
+# 10.1.1 국민계정(2015년 기준년) - 주요지표 - 연간지표 [111Y002] (1953 부터)
+BOK111Y002 = get_bok_data(STAT_CODE="111Y002", CYCLE_TYPE="YY", START_DATE="1953", END_DATE="2020")
+BOK111Y002.groupby(["ITEM_CODE1", "ITEM_NAME1"]).size()
+BOK111Y002_01 = BOK111Y002[BOK111Y002["ITEM_CODE1"] == "20101"].copy()  # 국내총생산(실질성장률)[%]
+
+# 12.1.1 기업경영분석 - 기업경영분석지표 - 기업경영분석지표(~2007)[027Y131][YY] (1960 부터)
+BOK027Y131 = get_bok_data(STAT_CODE="027Y131", CYCLE_TYPE="YY", START_DATE="1960", END_DATE="2006")
+BOK027Y131_01 = BOK027Y131[(BOK027Y131["ITEM_NAME1"] == "전산업") & (BOK027Y131["ITEM_NAME2"] == "매출액영업이익률")].copy()  # 국내총생산(실질성장률)[%]
+BOK027Y131_02 = BOK027Y131[(BOK027Y131["ITEM_NAME1"] == "제조업") & (BOK027Y131["ITEM_NAME2"] == "매출액영업이익률")].copy()  # 국내총생산(실질성장률)[%]
+BOK027Y131_03 = BOK027Y131[(BOK027Y131["ITEM_NAME1"] == "제조업") & (BOK027Y131["ITEM_NAME2"] == "매출액경상이익률(~2006)")].copy()  # 국내총생산(실질성장률)[%]
+
+# 12.1.1 기업경영분석 - 기업경영분석지표 - 기업경영분석지표(2007~2010)[027Y331][YY]
+BOK027Y331 = get_bok_data(STAT_CODE="027Y331", CYCLE_TYPE="YY", START_DATE="2007", END_DATE="2008")
+BOK027Y331_01 = BOK027Y331[(BOK027Y331["ITEM_NAME1"] == "전산업") & (BOK027Y331["ITEM_NAME2"] == "매출액영업이익률")].copy()  # 국내총생산(실질성장률)[%]
+BOK027Y331_02 = BOK027Y331[(BOK027Y331["ITEM_NAME1"] == "제조업") & (BOK027Y331["ITEM_NAME2"] == "매출액영업이익률")].copy()  # 국내총생산(실질성장률)[%]
+BOK027Y331_03 = BOK027Y331[(BOK027Y331["ITEM_NAME1"] == "제조업") & (BOK027Y331["ITEM_NAME2"] == "매출액세전순이익률")].copy()  # 국내총생산(실질성장률)[%]
+
+# 12.1.1 기업경영분석 - 기업경영분석지표 - 기업경영분석지표(2009~, 전수조사) [027Y431][YY]
+BOK027Y431 = get_bok_data(STAT_CODE="027Y431", CYCLE_TYPE="YY", START_DATE="2009", END_DATE="2020")
+BOK027Y431.groupby(["ITEM_CODE1", "ITEM_NAME1"]).size()
+BOK027Y431_01 = BOK027Y431[(BOK027Y431["ITEM_NAME1"] == "전산업") & (BOK027Y431["ITEM_NAME2"] == "매출액영업이익률")].copy()  # 국내총생산(실질성장률)[%]
+BOK027Y431_02 = BOK027Y431[(BOK027Y431["ITEM_NAME1"] == "제조업") & (BOK027Y431["ITEM_NAME2"] == "매출액영업이익률")].copy()  # 국내총생산(실질성장률)[%]
+BOK027Y431_03 = BOK027Y431[(BOK027Y431["ITEM_NAME1"] == "제조업") & (BOK027Y431["ITEM_NAME2"] == "매출액세전순이익률")].copy()  # 국내총생산(실질성장률)[%]
+
+########################################################################################################################
+# 그림: 경제성장률과 기업이익의 추이
+real_gdp_growth = BOK111Y002_01.copy()
+net_income_to_sales = pd.concat([BOK027Y131_03, BOK027Y331_03, BOK027Y431_03])
+
+fig, ax1 = plt.subplots()
+
+color1 = "tab:red"
+ax1.set_xlabel("year")
+ax1.set_ylabel("Real GDP Growth (annual, %)", color=color1)
+ax1.plot(real_gdp_growth["TIME"], real_gdp_growth["DATA_VALUE"], color=color1)
+ax1.tick_params(axis="y")
+
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+color2 = "tab:blue"
+ax2.set_ylabel("Net Income to Sales (annual, %)", color=color2)  # we already handled the x-label with ax1
+ax2.plot(net_income_to_sales["TIME"], net_income_to_sales["DATA_VALUE"], color=color2, linestyle='-')
+ax2.tick_params(axis='y')
+
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+fig.set_size_inches(2400/300, 1800/300)  # 그래프 크기 지정, DPI=300
+align_yaxis(ax1, 0, ax2, 0)  # 두 축이 동일한 0 값을 가지도록 조정
+plt.axhline(y=0, color='green', linestyle='dotted')
+plt.xlim([1981, 2020])
+plt.show()
+
+# 그림 저장
+plt.savefig("./data_processed/fig1_net_income_to_sales_and_real_gdp_growth.png")
+
+########################################################################################################################
+# 실질GDP (전년동기대비변동률, %)
+# 6.1.2 증권/재정 - 주식거래 및 주가지수 [028Y015][MM, YY] (200002, 1976 부터)
+BOK028Y015 = get_bok_data(STAT_CODE="028Y015", CYCLE_TYPE="YY", START_DATE="1976", END_DATE="2020")
+BOK028Y015.groupby(["ITEM_CODE1", "ITEM_NAME1"]).size()
+BOK028Y015_01 = BOK028Y015[BOK028Y015["ITEM_NAME1"] == "KOSPI_종가"].copy()  # 국내총생산(GDP)(실질, 원계열, 전년동기)
+
+kospi_index = BOK028Y015_01.copy()
+kospi_index["L1_DATA_VALUE"] = kospi_index["DATA_VALUE"].shift(1)
+kospi_index["GROWTH_RATE"] = (kospi_index["DATA_VALUE"]/kospi_index["L1_DATA_VALUE"]-1)*100
+
+fig, ax1 = plt.subplots()
+
+color1 = "tab:red"
+ax1.set_xlabel("year")
+ax1.set_ylabel("Real GDP Growth (annual, %)", color=color1)
+ax1.plot(real_gdp_growth["TIME"], real_gdp_growth["DATA_VALUE"], color=color1)
+ax1.tick_params(axis="y")
+
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+color2 = "tab:blue"
+ax2.set_ylabel("KOSPI Growth (annual, %)", color=color2)  # we already handled the x-label with ax1
+ax2.plot(kospi_index["TIME"], kospi_index["GROWTH_RATE"], color=color2, linestyle='-')
+ax2.tick_params(axis='y')
+
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+fig.set_size_inches(2400/300, 1800/300)  # 그래프 크기 지정, DPI=300
+align_yaxis(ax1, 0, ax2, 0)  # 두 축이 동일한 0 값을 가지도록 조정
+plt.axhline(y=0, color='green', linestyle='dotted')
+plt.xlim([1981, 2020])
+plt.show()
+
+# 그림 저장
+plt.savefig("./data_processed/fig2_kospi_growth_and_real_gdp_growth.png")
+
+
+########################################################################################################################
+# 그림: 경제성장률과 코스피지수의 추이
+
+
 
 # 실질GDP (전년동기대비변동률, %)
 # 10.1.2 국민계정(2015년 기준년) - 주요지표 - 분기지표 [111Y055] (1960Q1 부터)
