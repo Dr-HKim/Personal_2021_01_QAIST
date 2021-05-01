54275a36e88fee3bcb986185d5dba00404da4a5c
Dr-HKim <yui7890@gmail.com>
2021-05-02 02:37:19 +0900

    Research Note: - minor updates

---
+++ source_codes/main_20210430_import_BOK.py
@@ -0,0 +1,212 @@
+# Created by Kim Hyeongjun on 01/19/2021.
+# Copyright © 2021 dr-hkim.github.io. All rights reserved.
+# 조지프 엘리스, "경제를 읽는 기술 (Ahead of the Curve)"
+
+
+import datetime
+import numpy as np
+import pandas as pd
+import requests
+import matplotlib.pyplot as plt
+from bs4 import BeautifulSoup
+from data_raw.def_authentication import *
+
+
+def align_yaxis(ax1, v1, ax2, v2):
+    """adjust ax2 ylimit so that v2 in ax2 is aligned to v1 in ax1"""
+    _, y1 = ax1.transData.transform((0, v1))
+    _, y2 = ax2.transData.transform((0, v2))
+    inv = ax2.transData.inverted()
+    _, dy = inv.transform((0, 0)) - inv.transform((0, y1-y2))
+    miny, maxy = ax2.get_ylim()
+    ax2.set_ylim(miny+dy, maxy+dy)
+
+
+def get_bok_data(STAT_CODE, CYCLE_TYPE, START_DATE, END_DATE, AUTH_KEY=get_bok_auth_key(), REQ_TYPE="xml", LANG="kr",
+                 START_COUNT="1", END_COUNT="100000", ITEM_1="?", ITEM_2="?", ITEM_3="?"):
+    # 호출하려는 OpenAPI URL 정의
+    BOK_url = "".join(
+        ["http://ecos.bok.or.kr/api/StatisticSearch/", AUTH_KEY, "/", REQ_TYPE, "/", LANG, "/", START_COUNT, "/",
+         END_COUNT, "/", STAT_CODE, "/", CYCLE_TYPE, "/", START_DATE, "/", END_DATE, "/", ITEM_1, "/", ITEM_2, "/",
+         ITEM_3])
+    # 정의된 OpenAPI URL을 호출합니다.
+    BOK_response = requests.get(BOK_url)
+
+    # http 요청이 성공했을때 API의 리턴값 가져오기
+    if BOK_response.status_code == 200:
+        try:
+            BOK_xml = BeautifulSoup(BOK_response.text, "xml")
+            BOK_xml_row = BOK_xml.find_all("row")
+
+            list_STAT_CODE = []
+            list_STAT_NAME = []
+            list_ITEM_CODE1 = []
+            list_ITEM_NAME1 = []
+            list_ITEM_CODE2 = []
+            list_ITEM_NAME2 = []
+            list_ITEM_CODE3 = []
+            list_ITEM_NAME3 = []
+            list_UNIT_NAME = []
+            list_TIME = []
+            list_DATA_VALUE = []
+
+            for item in BOK_xml_row:
+                item_STAT_CODE = item.find("STAT_CODE").text
+                item_STAT_NAME = item.find("STAT_NAME").text
+                item_ITEM_CODE1 = item.find("ITEM_CODE1").text
+                item_ITEM_NAME1 = item.find("ITEM_NAME1").text
+                item_ITEM_CODE2 = item.find("ITEM_CODE2").text
+                item_ITEM_NAME2 = item.find("ITEM_NAME2").text
+                item_ITEM_CODE3 = item.find("ITEM_CODE3").text
+                item_ITEM_NAME3 = item.find("ITEM_NAME3").text
+                item_UNIT_NAME = item.find("UNIT_NAME").text
+                item_TIME = item.find("TIME").text
+                item_DATA_VALUE = item.find("DATA_VALUE").text
+
+                try:
+                    item_DATA_VALUE = float(item_DATA_VALUE)
+                except:
+                    item_DATA_VALUE = np.nan
+
+                list_STAT_CODE.append(item_STAT_CODE)
+                list_STAT_NAME.append(item_STAT_NAME)
+                list_ITEM_CODE1.append(item_ITEM_CODE1)
+                list_ITEM_NAME1.append(item_ITEM_NAME1)
+                list_ITEM_CODE2.append(item_ITEM_CODE2)
+                list_ITEM_NAME2.append(item_ITEM_NAME2)
+                list_ITEM_CODE3.append(item_ITEM_CODE3)
+                list_ITEM_NAME3.append(item_ITEM_NAME3)
+                list_UNIT_NAME.append(item_UNIT_NAME)
+                list_TIME.append(item_TIME)
+                list_DATA_VALUE.append(item_DATA_VALUE)
+
+            df_BOK = pd.DataFrame(list_STAT_CODE, columns=["STAT_CODE"])
+            df_BOK["STAT_NAME"] = list_STAT_NAME
+            df_BOK["ITEM_CODE1"] = list_ITEM_CODE1
+            df_BOK["ITEM_NAME1"] = list_ITEM_NAME1
+            df_BOK["ITEM_CODE2"] = list_ITEM_CODE2
+            df_BOK["ITEM_NAME2"] = list_ITEM_NAME2
+            df_BOK["ITEM_CODE3"] = list_ITEM_CODE3
+            df_BOK["ITEM_NAME3"] = list_ITEM_NAME3
+            df_BOK["UNIT_NAME"] = list_UNIT_NAME
+            df_BOK["TIME"] = list_TIME
+            df_BOK["TIME"] = df_BOK["TIME"].apply(pd.to_numeric)
+            df_BOK["DATA_VALUE"] = list_DATA_VALUE
+
+            if CYCLE_TYPE == "MM":
+                df_BOK["DATETIME"] = pd.to_datetime(df_BOK['TIME'].astype(str), errors='coerce', format='%Y%m')
+            elif CYCLE_TYPE == "DD":
+                df_BOK["DATETIME"] = pd.to_datetime(df_BOK['TIME'].astype(str), errors='coerce', format='%Y%m%d')
+
+        except Exception as e:
+            print(str(e))
+            # 예외가 발생했을때 처리
+    return df_BOK
+
+
+MM_START_DATE = "200301"
+QQ_START_DATE = "20031"
+
+DD_END_DATE = "20210219"
+MM_END_DATE = "202101"
+QQ_END_DATE = "20204"
+
+########################################################################################################################
+# 10.1.1 국민계정(2015년 기준년) - 주요지표 - 연간지표 [111Y002] (1953 부터)
+BOK111Y002 = get_bok_data(STAT_CODE="111Y002", CYCLE_TYPE="YY", START_DATE="1953", END_DATE="2020")
+BOK111Y002.groupby(["ITEM_CODE1", "ITEM_NAME1"]).size()
+BOK111Y002_01 = BOK111Y002[BOK111Y002["ITEM_CODE1"] == "20101"].copy()  # 국내총생산(실질성장률)[%]
+
+# 12.1.1 기업경영분석 - 기업경영분석지표 - 기업경영분석지표(~2007)[027Y131][YY] (1960 부터)
+BOK027Y131 = get_bok_data(STAT_CODE="027Y131", CYCLE_TYPE="YY", START_DATE="1960", END_DATE="2006")
+BOK027Y131_01 = BOK027Y131[(BOK027Y131["ITEM_NAME1"] == "전산업") & (BOK027Y131["ITEM_NAME2"] == "매출액영업이익률")].copy()  # 국내총생산(실질성장률)[%]
+BOK027Y131_02 = BOK027Y131[(BOK027Y131["ITEM_NAME1"] == "제조업") & (BOK027Y131["ITEM_NAME2"] == "매출액영업이익률")].copy()  # 국내총생산(실질성장률)[%]
+BOK027Y131_03 = BOK027Y131[(BOK027Y131["ITEM_NAME1"] == "제조업") & (BOK027Y131["ITEM_NAME2"] == "매출액경상이익률(~2006)")].copy()  # 국내총생산(실질성장률)[%]
+
+# 12.1.1 기업경영분석 - 기업경영분석지표 - 기업경영분석지표(2007~2010)[027Y331][YY]
+BOK027Y331 = get_bok_data(STAT_CODE="027Y331", CYCLE_TYPE="YY", START_DATE="2007", END_DATE="2008")
+BOK027Y331_01 = BOK027Y331[(BOK027Y331["ITEM_NAME1"] == "전산업") & (BOK027Y331["ITEM_NAME2"] == "매출액영업이익률")].copy()  # 국내총생산(실질성장률)[%]
+BOK027Y331_02 = BOK027Y331[(BOK027Y331["ITEM_NAME1"] == "제조업") & (BOK027Y331["ITEM_NAME2"] == "매출액영업이익률")].copy()  # 국내총생산(실질성장률)[%]
+BOK027Y331_03 = BOK027Y331[(BOK027Y331["ITEM_NAME1"] == "제조업") & (BOK027Y331["ITEM_NAME2"] == "매출액세전순이익률")].copy()  # 국내총생산(실질성장률)[%]
+
+# 12.1.1 기업경영분석 - 기업경영분석지표 - 기업경영분석지표(2009~, 전수조사) [027Y431][YY]
+BOK027Y431 = get_bok_data(STAT_CODE="027Y431", CYCLE_TYPE="YY", START_DATE="2009", END_DATE="2020")
+BOK027Y431.groupby(["ITEM_CODE1", "ITEM_NAME1"]).size()
+BOK027Y431_01 = BOK027Y431[(BOK027Y431["ITEM_NAME1"] == "전산업") & (BOK027Y431["ITEM_NAME2"] == "매출액영업이익률")].copy()  # 국내총생산(실질성장률)[%]
+BOK027Y431_02 = BOK027Y431[(BOK027Y431["ITEM_NAME1"] == "제조업") & (BOK027Y431["ITEM_NAME2"] == "매출액영업이익률")].copy()  # 국내총생산(실질성장률)[%]
+BOK027Y431_03 = BOK027Y431[(BOK027Y431["ITEM_NAME1"] == "제조업") & (BOK027Y431["ITEM_NAME2"] == "매출액세전순이익률")].copy()  # 국내총생산(실질성장률)[%]
+
+########################################################################################################################
+# 그림: 경제성장률과 기업이익의 추이
+real_gdp_growth = BOK111Y002_01.copy()
+net_income_to_sales = pd.concat([BOK027Y131_03, BOK027Y331_03, BOK027Y431_03])
+
+fig, ax1 = plt.subplots()
+
+color1 = "tab:red"
+ax1.set_xlabel("year")
+ax1.set_ylabel("Real GDP Growth (annual, %)", color=color1)
+ax1.plot(real_gdp_growth["TIME"], real_gdp_growth["DATA_VALUE"], color=color1)
+ax1.tick_params(axis="y")
+
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+color2 = "tab:blue"
+ax2.set_ylabel("Net Income to Sales (annual, %)", color=color2)  # we already handled the x-label with ax1
+ax2.plot(net_income_to_sales["TIME"], net_income_to_sales["DATA_VALUE"], color=color2, linestyle='-')
+ax2.tick_params(axis='y')
+
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+fig.set_size_inches(2400/300, 1800/300)  # 그래프 크기 지정, DPI=300
+align_yaxis(ax1, 0, ax2, 0)  # 두 축이 동일한 0 값을 가지도록 조정
+plt.axhline(y=0, color='green', linestyle='dotted')
+plt.xlim([1981, 2020])
+plt.show()
+
+# 그림 저장
+plt.savefig("./data_processed/fig1_net_income_to_sales_and_real_gdp_growth.png")
+
+########################################################################################################################
+# 실질GDP (전년동기대비변동률, %)
+# 6.1.2 증권/재정 - 주식거래 및 주가지수 [028Y015][MM, YY] (200002, 1976 부터)
+BOK028Y015 = get_bok_data(STAT_CODE="028Y015", CYCLE_TYPE="YY", START_DATE="1976", END_DATE="2020")
+BOK028Y015.groupby(["ITEM_CODE1", "ITEM_NAME1"]).size()
+BOK028Y015_01 = BOK028Y015[BOK028Y015["ITEM_NAME1"] == "KOSPI_종가"].copy()  # 국내총생산(GDP)(실질, 원계열, 전년동기)
+
+# 그림: 경제성장률과 코스피지수의 추이
+kospi_index = BOK028Y015_01.copy()
+kospi_index["L1_DATA_VALUE"] = kospi_index["DATA_VALUE"].shift(1)
+kospi_index["GROWTH_RATE"] = (kospi_index["DATA_VALUE"]/kospi_index["L1_DATA_VALUE"]-1)*100
+
+fig, ax1 = plt.subplots()
+
+color1 = "tab:red"
+ax1.set_xlabel("year")
+ax1.set_ylabel("Real GDP Growth (annual, %)", color=color1)
+ax1.plot(real_gdp_growth["TIME"], real_gdp_growth["DATA_VALUE"], color=color1)
+ax1.tick_params(axis="y")
+
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+color2 = "tab:blue"
+ax2.set_ylabel("KOSPI Growth (annual, %)", color=color2)  # we already handled the x-label with ax1
+ax2.plot(kospi_index["TIME"], kospi_index["GROWTH_RATE"], color=color2, linestyle='-')
+ax2.tick_params(axis='y')
+
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+fig.set_size_inches(2400/300, 1800/300)  # 그래프 크기 지정, DPI=300
+align_yaxis(ax1, 0, ax2, 0)  # 두 축이 동일한 0 값을 가지도록 조정
+plt.axhline(y=0, color='green', linestyle='dotted')
+plt.xlim([1981, 2020])
+plt.show()
+
+# 그림 저장
+plt.savefig("./data_processed/fig2_kospi_growth_and_real_gdp_growth.png")
+
+
+########################################################################################################################
+kosis_url = get_kosis_url()
+kosis_response = requests.get(kosis_url)  # Open API URL 호출
+data = kosis_response.json()
+dfItem = pd.DataFrame.from_records(data)
+
---
+++ source_codes/main_20210501_import_OECD_stats.py
@@ -0,0 +1,189 @@
+# Created by Kim Hyeongjun on 05/01/2021.
+# Copyright © 2021 dr-hkim.github.io. All rights reserved.
+# 참고: https://stackoverflow.com/questions/40565871/read-data-from-oecd-api-into-python-and-pandas
+# 참고: https://pandas-datareader.readthedocs.io/en/latest/remote_data.html#remote-data-oecd (Fama-French)
+
+import requests
+import pandas as pd
+import re
+import matplotlib.pyplot as plt
+
+
+def get_yyyymm_add_months(n_yyyymm, n_months):
+    n_yyyy, n_mm = divmod(n_yyyymm, 100)
+    n_months_y, n_months_m = divmod(n_mm + n_months - 1, 12)
+    output_yyyy = n_yyyy + n_months_y
+    output_mm = n_months_m + 1
+    output_yyyymm = output_yyyy * 100 + output_mm
+    return output_yyyymm
+
+
+def align_yaxis(ax1, v1, ax2, v2):
+    """adjust ax2 ylimit so that v2 in ax2 is aligned to v1 in ax1"""
+    _, y1 = ax1.transData.transform((0, v1))
+    _, y2 = ax2.transData.transform((0, v2))
+    inv = ax2.transData.inverted()
+    _, dy = inv.transform((0, 0)) - inv.transform((0, y1-y2))
+    miny, maxy = ax2.get_ylim()
+    ax2.set_ylim(miny+dy, maxy+dy)
+
+
+def get_oecd_response(dataset, subjects, countries, frequency, startDate, endDate,
+                      root_dir="https://stats.oecd.org/SDMX-JSON/data"):
+    # Make URL for the OECD API and return a response
+    # 4 dimensions: SUBJECT, LOCATION, FREQUENCY, TIME_PERIOD
+    # OECD API: https://data.oecd.org/api/sdmx-json-documentation/#d.en.330346
+
+    dimension = [subjects, countries, frequency]
+    dimension_args = ['+'.join(d) for d in dimension]
+    dimension_str = ".".join(dimension_args)
+
+    url = root_dir + '/' + dataset + '/' + dimension_str + "/all?startTime=" + startDate + "&endTime=" + endDate + \
+          "&dimensionAtObservation=allDimensions"
+
+    print('Requesting URL ' + url)
+    return requests.get(url=url)
+
+
+def get_oecd_data(response):
+    # Request data from OECD API and return pandas DataFrame
+
+    # country: country code (max 1)
+    # subject: list of subjects, empty list for all
+    # measure: list of measures, empty list for all
+    # frequency: 'M' for monthly and 'Q' for quarterly time series
+    # startDate: date in YYYY-MM (2000-01) or YYYY-QQ (2000-Q1) format, None for all observations
+    # endDate: date in YYYY-MM (2000-01) or YYYY-QQ (2000-Q1) format, None for all observations
+
+    # Data download
+    # response = requests.get(url)  # Open API URL 호출
+
+    # Data transformation
+    if (response.status_code == 200):
+        responseJson = response.json()
+        obsList = responseJson.get('dataSets')[0].get('observations')
+
+        if (len(obsList) > 0):
+            print('Data downloaded from %s' % response.url)
+
+            subjectList = [
+                item for item in responseJson.get('structure').get('dimensions').get('observation') if
+                item['id'] == 'SUBJECT'][0]['values']
+            locationList = [
+                item for item in responseJson.get('structure').get('dimensions').get('observation') if
+                item['id'] == 'LOCATION'][0]['values']
+            frequencyList = [
+                item for item in responseJson.get('structure').get('dimensions').get('observation') if
+                item['id'] == 'FREQUENCY'][0]['values']
+            timeList = [
+                item for item in responseJson.get('structure').get('dimensions').get('observation') if
+                item['id'] == 'TIME_PERIOD'][0]['values']
+
+            obs = pd.DataFrame(obsList).transpose()
+            obs.rename(columns={0: 'series'}, inplace=True)
+            obs['id'] = obs.index
+            obs = obs[['id', 'series']]
+            obs['dimensions'] = obs.apply(lambda x: re.findall('\d+', x['id']), axis=1)
+            obs_dim_expanded = obs["dimensions"].apply(pd.Series)
+            obs_dim_expanded.columns = ["dim0", "dim1", "dim2", "dim3"]
+            obs = pd.merge(obs, obs_dim_expanded, left_index=True, right_index=True, how='outer')
+
+            obs['subject_id'] = obs.apply(lambda x: subjectList[int(x["dim0"])]['id'], axis=1)
+            obs['subject_name'] = obs.apply(lambda x: subjectList[int(x["dim0"])]['name'], axis=1)
+            obs['location_id'] = obs.apply(lambda x: locationList[int(x["dim1"])]['id'], axis=1)
+            obs['location_name'] = obs.apply(lambda x: locationList[int(x["dim1"])]['name'], axis=1)
+            obs['time'] = obs.apply(lambda x: timeList[int(x["dim3"])]['id'], axis=1)
+            obs['names'] = obs['subject_id'].copy()  # + '_' + obs['measure']
+            obs.reset_index(level=0, inplace=True)
+            obs.rename(columns={"series": "datavalue"}, inplace=True)
+
+            obs["yyyy"] = obs["time"].str.slice(start=0, stop=4).astype(int)
+            obs["mm"] = obs["time"].str.slice(start=5, stop=7).astype(int)
+            obs["yyyymm"] = obs["yyyy"] * 100 + obs["mm"]
+
+            data = obs[["yyyymm", "subject_id", "subject_name", "location_id", "location_name", "datavalue"]]
+
+            # data = obs.pivot_table(index='time', columns=['names'], values='series')
+            return data
+
+        else:
+            print('Error: No available records, please change parameters')
+    else:
+        print('Error: %s' % response.status_code)
+
+
+# OECD 데이터 -> export -> Developer API -> Generate API queries
+
+oecd_response = get_oecd_response(
+    dataset="MEI_CLI",
+    subjects=["LOLITOAA", "LOLITONO", "LOLITOTR_STSA", "LOLITOTR_GYSA", "BSCICP03", "CSCICP03",
+              "LORSGPRT", "LORSGPNO", "LORSGPTD", "LORSGPOR_IXOBSA"],
+    countries=["AUS", "AUT", "BEL", "CAN", "CHL", "CZE", "DNK", "EST", "FIN", "FRA", "DEU", "GRC", "HUN",
+               "ISL", "IRL", "ISR", "ITA", "JPN", "KOR", "LUX", "MEX", "NLD", "NZL", "NOR", "POL", "PRT",
+               "SVK", "SVN", "ESP", "SWE", "CHE", "TUR", "GBR", "USA", "EA19", "G4E", "G-7", "NAFTA",
+               "OECDE", "OECD", "ONM", "A5M", "BRA", "CHN", "IND", "IDN", "RUS", "ZAF"],
+    frequency="M", startDate="1990-01", endDate="2021-03"
+)
+
+df_oecd = get_oecd_data(oecd_response)
+counts_subjects = df_oecd[["subject_id", "subject_name"]].value_counts()
+counts_location = df_oecd["location_name"].value_counts()
+
+# LOLITOTR_GYSA: 12-month rate of change of the trend restored CLI
+df_oecd_cli = df_oecd[(df_oecd["location_id"] == "OECD") & (df_oecd["subject_id"] == "LOLITOTR_GYSA")].copy()
+
+# YYYYMM 을 기준으로 그 달의 가장 마지막 날짜 입력
+df_oecd_cli["Date"] = pd.to_datetime(
+    get_yyyymm_add_months(df_oecd_cli["yyyymm"], 1) * 100 + 1, errors='coerce', format='%Y%m%d') + pd.Timedelta(days=-1)
+
+########################################################################################################################
+# MSCI 데이터 불러오기
+df_index_daily = pd.read_pickle('./data_processed/df_index_daily_20210430.pkl')
+
+# Daily to Monthly
+# 날짜를 YYYYMM 형태로 변환
+df_index_daily["YYYYMM"] = df_index_daily["Date"].dt.year * 100 + df_index_daily["Date"].dt.month
+
+# YYYYMM 그룹별 OHLC 구하기
+df_index_monthly = df_index_daily.groupby(by='YYYYMM', as_index=False).agg({
+    "MXEF_Close": "last", "MXEF_Open": "first", "MXEF_High": "max", "MXEF_Low": "min",
+    "MXWO_Close": "last", "MXWO_Open": "first", "MXWO_High": "max", "MXWO_Low": "min",
+    "KOSPI_Close": "last", "KOSPI_Open": "first", "KOSPI_High": "max", "KOSPI_Low": "min"})
+
+df_index_monthly["L12_MXEF_Close"] = df_index_monthly["MXEF_Close"].shift(12)  # lag
+df_index_monthly["MXEF_YoY"] = (df_index_monthly["MXEF_Close"]/df_index_monthly["L12_MXEF_Close"] - 1)*100
+df_index_monthly["L12_KOSPI_Close"] = df_index_monthly["KOSPI_Close"].shift(12)  # lag
+df_index_monthly["KOSPI_YoY"] = (df_index_monthly["KOSPI_Close"]/df_index_monthly["L12_KOSPI_Close"] - 1)*100
+
+# YYYYMM 을 기준으로 그 달의 가장 마지막 날짜 입력
+df_index_monthly["Date"] = pd.to_datetime(
+    get_yyyymm_add_months(df_index_monthly["YYYYMM"], 1) * 100 + 1, errors='coerce', format='%Y%m%d') + pd.Timedelta(days=-1)
+
+
+########################################################################################################################
+# 그림: OECD Composite leading indicator and MSCI Emerging Markets
+
+fig, ax1 = plt.subplots()
+
+color1 = "tab:red"
+ax1.set_xlim([pd.to_datetime("1990-01-01 00:00:00"), pd.to_datetime("2021-03-31 00:00:00")])
+ax1.set_xlabel("Date")
+ax1.set_ylabel("OECD Composite Leading Indicator", color=color1)
+ax1.plot(df_oecd_cli["Date"], df_oecd_cli["datavalue"], color=color1)
+ax1.tick_params(axis="y")
+
+ax2 = ax1.twinx()  # instantiate a second axes that shares the same x-axis
+
+color2 = "tab:blue"
+ax2.set_ylabel("MSCI Emerging Markets Monthly YoY", color=color2)  # we already handled the x-label with ax1
+ax2.plot(df_index_monthly["Date"], df_index_monthly["MXEF_YoY"], color=color2, linestyle='-')
+ax2.tick_params(axis='y')
+
+fig.tight_layout()  # otherwise the right y-label is slightly clipped
+fig.set_size_inches(2400/300, 1800/300)  # 그래프 크기 지정, DPI=300
+align_yaxis(ax1, 0, ax2, 0)  # 두 축이 동일한 0 값을 가지도록 조정
+plt.axhline(y=0, color='green', linestyle='dotted')
+plt.show()
+
+# 그림 저장
+plt.savefig("./data_processed/fig4_oecd_cli_and_msci_emerging_markets.png")
