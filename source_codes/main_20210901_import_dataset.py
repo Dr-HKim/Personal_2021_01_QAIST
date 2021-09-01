d39ac67eb3630af88f8b443e7d10686698f0420f
HKim <yui7890@gmail.com>
2021-09-01 22:05:10 +0900

    Research Note: - minor updates

---
+++ source_codes/main_20210901_import_dataset.py
@@ -0,0 +1,7 @@
+# Import Packages
+import pandas as pd
+
+
+########################################################################################################################
+# Import Pickle Dataset
+dg_GeneralHistoric_Quarterly = pd.read_pickle('./data_processed/dg_GeneralHistoric_Quarterly.pkl')
